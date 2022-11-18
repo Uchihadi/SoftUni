a70a2949b4d1bdad7755ad4da9a1e282c8aedaa3
Uchihadi <adilovescode@gmail.com>
2022-11-19 01:42:30 +0800

    Exams

---
+++ src/AdvancedConditions/Exam/PoolDay.java
@@ -0,0 +1,24 @@
+package AdvancedConditions.Exam;
+
+import java.util.Scanner;
+
+public class PoolDay {
+    public static void main(String[] args) {
+        // Write code here
+        Scanner scanner = new Scanner(System.in);
+        int ppl = Integer.parseInt(scanner.nextLine());
+        double feePerson = Double.parseDouble(scanner.nextLine());
+        double priceSunbed = Double.parseDouble(scanner.nextLine());
+        double priceUmbrella = Double.parseDouble(scanner.nextLine());
+
+        double umbrellaNeeded = Math.ceil(ppl/2.0);
+        double sunbedNeeded = Math.ceil(0.75*ppl);
+
+        double entranceFee = ppl * feePerson;
+        double umbrellaFee = umbrellaNeeded * priceUmbrella;
+        double sunbedFee = sunbedNeeded * priceSunbed;
+        double totalCost = entranceFee + umbrellaFee + sunbedFee;
+
+        System.out.printf("%.2f euro", totalCost);
+    }
+}
---
+++ src/AdvancedConditions/Exam/TournamentChristmas.java
@@ -0,0 +1,7 @@
+package AdvancedConditions.Exam;
+
+public class TournamentChristmas {
+    public static void main(String[] args) {
+        // Write code here
+    }
+}
