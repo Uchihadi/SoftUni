0a86cf4741b7c4828f94459c3b3f47f4c06fac53
Uchihadi <adilovescode@gmail.com>
2022-11-19 08:52:14 +0800

    DataTypes

---
+++ src/DataTypes/Homework/SpiceMustFlow.java
@@ -0,0 +1,27 @@
+package DataTypes.Homework;
+
+import java.util.Scanner;
+
+public class SpiceMustFlow {
+    public static void main(String[] args) {
+        // Write code here
+        Scanner scanner = new Scanner(System.in);
+        int currentYield = Integer.parseInt(scanner.nextLine());
+
+        int day = 0;
+        int extracted = 0;
+
+        while (true){
+            day++;
+            extracted += currentYield-26;
+            currentYield -= 10;
+
+            if (currentYield < 100){
+                break;
+            }
+        }
+        extracted -= 26;
+        System.out.println(day);
+        System.out.println(extracted);
+    }
+}
