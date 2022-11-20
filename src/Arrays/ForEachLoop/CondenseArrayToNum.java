b28869c2c7d7a6fc63d14e1be756460f935bbb91
Uchihadi <adilovescode@gmail.com>
2022-11-20 22:03:23 +0800

    arrays

--- src/Arrays/ForEachLoop/CondenseArrayToNum.java
+++ src/Arrays/ForEachLoop/CondenseArrayToNum.java
@@ -10,14 +10,18 @@ public class CondenseArrayToNum {
                 .mapToInt(e -> Integer.parseInt(e))
                 .toArray();
 
+        // Access length property; if array length = 1 then print the position 0 of the array
+        // If not
         if (nums.length == 1) {
             System.out.println(nums[0]);
         } else {
             while (nums.length != 1) {
                 int[] condensed = new int[nums.length - 1];
-                for (int i = 0; i < nums.length - 1; i++){
+                for (int i = 0; i < condensed.length; i++){
                     condensed[i] = nums[i] + nums[i + 1];
                 }
+
+                // Update nums array and access the condensed array to nums
                 nums = condensed;
             }
             System.out.println(nums[0]);
--- src/Arrays/Homework/TopIntegers.java
+++ src/Arrays/Homework/TopIntegers.java
@@ -1,8 +1,32 @@
 package Arrays.Homework;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class TopIntegers {
     public static void main(String[] args) {
         // Write your code here
+        Scanner scanner = new Scanner(System.in);
 
-    }
+        // Input an array of numbers with spaces in between
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+
+        // Starts with first number of the Array
+        for (int i = 0; i < numbers.length; i++){
+
+            // Created a boolean condition
+            boolean top = true;
+
+            // Second number of the Array
+            for (int j = i + 1; j < numbers.length; j++){
+                if (numbers[i] <= numbers[j]) {
+                    top = false; // Eliminates all the lesser numbers by 2 arrays
+                }
+            }
+            if (top){
+                    System.out.print(numbers[i] + " ");
+                }
+            }
+        }
 }
