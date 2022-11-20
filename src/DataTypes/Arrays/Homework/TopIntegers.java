512c89db78c921ed635d6c92cd4ac0ead65b0aa7
Uchihadi <adilovescode@gmail.com>
2022-11-20 14:00:24 +0800

    ArrayExercises

---
+++ src/DataTypes/Arrays/Homework/TopIntegers.java
@@ -0,0 +1,11 @@
+package DataTypes.Arrays.Homework;
+
+import java.util.Scanner;
+
+public class TopIntegers {
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner scanner = new Scanner(System.in);
+        String[] arr
+    }
+}
