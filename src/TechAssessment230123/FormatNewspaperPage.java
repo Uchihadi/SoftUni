dad05bad0ca5acc3207a1efcb42141b09d6f5811
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-01-24 04:15:45 +0800

    Update FormatNewspaperPage.java

--- src/TechAssessment230123/FormatNewspaperPage.java
+++ src/TechAssessment230123/FormatNewspaperPage.java
@@ -3,7 +3,14 @@ package TechAssessment230123;
 import java.util.ArrayList;
 import java.util.List;
 
-//
+// You are formatting the Text on a Newspaper Page. You must align the text on Page properly
+// Texts are provided to you in following formats:
+// 1) Paragraphs (An array of paragraphs where each paragraph is represented as an array containing portions of text)
+// 2) Widths (Representing the maximum number of characters on each line of the newspaper page can include)
+// Your task is to produce a Newspaper Page according to following specifications:
+// i) Start a new line for each paragraph, paragraphs[i][j] in order. When words are together on a line, they should be separated by 1 space
+// (Every Paragraph word Paragraphs[i][j] is guaranteed to contain only lowercase and uppercase English letters)
+// Return the resulting Newspaper Page as an Array of Strings. It is possible to format Paragraphs as described
 
 public class FormatNewspaperPage {
     public static String[] formatNewspaper(String[][] paragraphs, int width) {
