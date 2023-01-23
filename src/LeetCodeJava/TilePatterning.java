134b37aff68bedf1f426239b26d5f2e6233281b7
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-01-23 17:11:31 +0800

    Update TilePatterning.java

--- src/LeetCodeJava/TilePatterning.java
+++ src/LeetCodeJava/TilePatterning.java
@@ -10,15 +10,15 @@ public class TilePatterning {
     }
 
     private static int dfs(int[] arr) { // Function DFS takes Array as Inputs, where each element of arrays represents number of occurrences
-        int sum = 0; 
+        int sum = 0; // Sum Count starts from 0
         for (int i = 0; i < 26; i++) { // Generates all possible permutations of letters
             if (arr[i] == 0) {
-                continue;
+                continue; // If Array is 0, it will break out of the loop
             }
-            sum++; // Increments a Counter Variable 'sum' by one
-            arr[i]--;
-            sum += dfs(arr);
-            arr[i]++;
+            sum++; // Increments a Counter Variable 'sum' by one for each letter in the Array
+            arr[i]--; // Decrements the value to show that one instance of letter has been used
+            sum += dfs(arr); // Recursively calls function DFS with updated array, generates all possible permutations of remaining letters in Array
+            arr[i]++; // Increments value of current element, restores the Original State of Array. Continues to Next Element in the Array
         }
         return sum; // Final value of sum is returned as Output of the Function
     }
