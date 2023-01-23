14f65dcd3898dfbffe8447230b7fc2d606abdc7d
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-01-24 03:55:57 +0800

    Update PlayerHealthValues.java

--- src/TechAssessment230123/PlayerHealthValues.java
+++ src/TechAssessment230123/PlayerHealthValues.java
@@ -1,5 +1,13 @@
 package TechAssessment230123;
 
+// Imagine a Video Game where Player Controls a Character to go through several levels
+// Character has initial health values of initialHealth, value changes as Player goes through levels
+// You are given an array of Integers deltas, defining each health value changes
+// The ith level (0 - indexed) changes character's current health value by deltas[i]
+// Whenever health value is less than 0, it becomes 0. Similarly, if it is more than 100, then health is 100
+// Task is to return the character's final Health value after going through all levels in the game
+// Eg. initialHealth = 12 & deltas = [-4, -12, 6, 2] then output (initialHealth, deltas) = 8
+
 public class PlayerHealthValues {
     int solution(int initialHealth, int[] deltas) {
         int currentHealth = initialHealth;
