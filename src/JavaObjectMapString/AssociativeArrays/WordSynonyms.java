1ef5513d2d25888330fcf9683d551805e12c9104
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-01-25 03:09:48 +0800

    WordSynonyms

--- src/JavaObjectMapString/AssociativeArrays/WordSynonyms.java
+++ src/JavaObjectMapString/AssociativeArrays/WordSynonyms.java
@@ -1,4 +1,22 @@
 package JavaObjectMapString.AssociativeArrays;
 
+import java.util.*;
+
 public class WordSynonyms {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine()); // How many inputs of synonyms you would like
+        LinkedHashMap<String, List<String>> words = new LinkedHashMap<>(); // LinkedHashMaps are for unique values
+
+        for(int i = 0; i < n; i++) {
+            String word = scanner.nextLine();
+            String synonym = scanner.nextLine();
+            words.putIfAbsent(word, new ArrayList<>()); // Creates a new ArrayList key for a word that is absent
+            words.get(word).add(synonym); // Adding synonym inside an ArrayList
+        }
+
+        for (Map.Entry<String, List<String>> entry : words.entrySet()){
+            System.out.printf("%s - %s%n", entry.getKey(), String.join(", ", entry.getValue()));
+        }
+    }
 }
