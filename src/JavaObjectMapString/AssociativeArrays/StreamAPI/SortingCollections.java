eef7f92c7e61d441f550269c897151a3555a16be
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-01-25 16:19:32 +0800

    Update SortingCollections.java

--- src/JavaObjectMapString/AssociativeArrays/StreamAPI/SortingCollections.java
+++ src/JavaObjectMapString/AssociativeArrays/StreamAPI/SortingCollections.java
@@ -1,7 +1,20 @@
 package JavaObjectMapString.AssociativeArrays.StreamAPI;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class SortingCollections {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> nums = new ArrayList<>();
+        nums.add(13);
+        nums.add(45);
+        nums.add(22);
+
+        List<Integer> ascending = nums.stream()
+                .sorted()((n1, n2) -> n1.compareTo(n2))
+
     }
 }
