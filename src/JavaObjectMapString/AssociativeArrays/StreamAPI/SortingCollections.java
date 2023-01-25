56f5b63954ca1187fde9e9f01f97097b8750e777
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-01-25 20:32:25 +0800

    sorting collections

--- src/JavaObjectMapString/AssociativeArrays/StreamAPI/SortingCollections.java
+++ src/JavaObjectMapString/AssociativeArrays/StreamAPI/SortingCollections.java
@@ -3,6 +3,7 @@ package JavaObjectMapString.AssociativeArrays.StreamAPI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class SortingCollections {
     public static void main(String[] args) {
@@ -13,8 +14,21 @@ public class SortingCollections {
         nums.add(45);
         nums.add(22);
 
-        List<Integer> ascending = nums.stream()
-                .sorted()((n1, n2) -> n1.compareTo(n2))
+        List<Integer> ascending = nums.stream() // Creating an ascending ArrayList nums stream
+                .sorted((n1, n2) -> n1.compareTo(n2))
+                .collect(Collectors.toList()); // Collect and accumulate all elements into a new List
 
+        for (Integer integer : ascending) {
+            System.out.println(integer); // Printing out the integer in ascending order
+        }
+
+        List<Integer> descending = nums.stream() // Creating an descending ArrayList nums stream
+                .sorted((n1, n2) -> n2.compareTo(n1))
+                .collect(Collectors.toList()); // Collect and accumulate all elements into a new List
+
+        System.out.println();
+        for (Integer integer : descending) {
+            System.out.println(integer); // Printing out the integer in descending order
+        }
     }
 }
