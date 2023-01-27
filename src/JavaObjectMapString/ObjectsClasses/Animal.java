975411135a16747c4708515fc9bb052f10f0b309
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-01-27 20:56:12 +0800

    Create Animal.java

---
+++ src/JavaObjectMapString/ObjectsClasses/Animal.java
@@ -0,0 +1,11 @@
+package JavaObjectMapString.ObjectsClasses;
+
+public class Animal {
+    public Animal(String mammal) {
+    }
+
+    public static void main(String[] args) {
+        Animal dog = new Animal("mammal");
+        Animal lizard = new Animal("reptile");
+    }
+}
