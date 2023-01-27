b0125a10b6f63e5909c4325f171ef4ca2f484036
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-01-27 21:13:18 +0800

    Static

---
+++ src/JavaObjectMapString/ObjectsClasses/StaticClass.java
@@ -0,0 +1,9 @@
+package JavaObjectMapString.ObjectsClasses;
+
+public class StaticClass {
+    public static void main(String[] args) {
+        double result = Math.pow(2, 3);
+        System.out.println(result);
+        // Example of built-in API class in Java
+    }
+}
