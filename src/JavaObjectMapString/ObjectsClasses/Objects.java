8302f6272aa30307e17c2f801c32d39604782254
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-01-27 20:50:07 +0800

    Create Objects.java

---
+++ src/JavaObjectMapString/ObjectsClasses/Objects.java
@@ -0,0 +1,9 @@
+package JavaObjectMapString.ObjectsClasses;
+
+import java.time.LocalDate;
+
+public class Objects {
+    public static void main(String[] args){
+        LocalDate birthday = LocalDate.of(2018, 5, 5);// Creating new object of type LocalDate
+    }
+}
