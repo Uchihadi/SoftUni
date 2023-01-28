10485cf9ac27ef884591e18cb12e4630a77a7f48
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-01-28 15:18:16 +0800

    BigInteger

---
+++ src/JavaObjectMapString/ObjectsClasses/BuiltInAPI/BigInteger.java
@@ -0,0 +1,7 @@
+package JavaObjectMapString.ObjectsClasses.BuiltInAPI;
+
+public class BigInteger {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/JavaObjectMapString/ObjectsClasses/BuiltInAPI/RandomizeWords.java
+++ src/JavaObjectMapString/ObjectsClasses/BuiltInAPI/RandomizeWords.java
@@ -8,14 +8,16 @@ public class RandomizeWords {
         Scanner scanner = new Scanner(System.in);
         String[] words = scanner.nextLine().split(" "); // Create an array
         Random random = new Random(); // Class Random with its object random
+        // Java Random class is used to generate a stream of pseudorandom numbers
 
         for (int i = 0; i < words.length; i++) {
-            int j = random.nextInt(words.length);
+            int j = random.nextInt(words.length); // To Swap them in a random order
             String wordToSwap = words[i]; // The word in an ith position of the Array
             words[i] = words[j]; // Element index i = index j
             words[j] = wordToSwap; // Swap element index j to the object wordToSwap
         }
 
         System.out.println(String.join(System.lineSeparator(), words));
+        // lineSeparator() method of Java System class returns line separator string which is system dependent
     }
 }
